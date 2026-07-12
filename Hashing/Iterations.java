01e5f002f0be5c44a5e791d8eb81caa357fad455
Anuj Kulkarni <kulkarnianuj10@gmail.com>
2026-07-12 19:57:32 +0530

    Add files via upload

---
+++ Hashing/Iterations.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+import java.util.HashMap;
+
+public class Iterations {
+    public static void main(String[] args) {
+        HashMap<String, Integer> hm = new HashMap<>();
+
+        hm.put("India", 100);
+        hm.put("China", 150);
+        hm.put("USA", 50);
+        hm.put("Indonesia", 6);
+        hm.put("Nepal", 5);
+        
+        Set<String> keys = hm.keySet();
+
+        System.out.println(keys);
+
+        for (String k : keys) { // foreach loop
+            System.out.println("Key="+ k + ", Value="+ hm.get(k));
+        }
+    }
+}
