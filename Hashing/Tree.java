bdc872a3a305041c8075c4f2a359fd230adc4af0
Anuj Kulkarni <kulkarnianuj10@gmail.com>
2026-07-13 20:03:50 +0530

    Add files via upload

---
+++ Hashing/Tree.java
@@ -0,0 +1,13 @@
+
+import java.util.TreeSet;
+
+public class Tree {
+    public static void main(String[] args) {
+        TreeSet<String> cities = new TreeSet<>(); // works a like Red-Black Tree
+
+        cities.add("Nashik");
+        cities.add("Dhule");
+
+        System.out.println(cities); // 
+    }
+}
