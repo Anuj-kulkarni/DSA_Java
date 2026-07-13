1b5e9a0f94e27da529ba81969b01d408b26598b9
Anuj Kulkarni <kulkarnianuj10@gmail.com>
2026-07-13 20:03:33 +0530

    Add files via upload

---
+++ Hashing/TreeMapExample.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class TreeMapExample {
+    public static void main(String[] args) {
+        TreeMap<String, Integer> tm = new TreeMap<>();
+        tm.put("India", 100);
+        tm.put("USA", 200);
+
+        System.out.println(tm);
+    }
+}
