2c5e5ccf52f6f0cb463176b83353c880d2e589ab
Anuj Kulkarni <kulkarnianuj10@gmail.com>
2026-08-03 21:54:45 +0530

    Refactor valid class to validParatheses and fix logic

--- Stack/valid.java
+++ Stack/valid.java
@@ -1,3 +1,32 @@
-public class valid {
-    
+import java.util.*;
+public class validParatheses {
+    public static void main(String[] args) {
+        
+        Stack<Character> s1 = new Stack<>();
+
+        char arr[] = {'(', ')', '{', '}'};
+
+        for(int i=0; i<arr.length; i++){
+            if(arr[i] =='(' || arr[i] == '[' || arr[i] == '{'){
+                s1.push(arr[i]);
+            }else{
+                if(s1.isEmpty()){
+                    // System.out.println("False");
+                    continue;
+                }
+                char top = s1.pop();
+                if(arr[i] ==')' && top == '(' || arr[i] == ']' && top == '[' || arr[i] == '}' && top == '{'){
+                    // System.out.println("False");
+                    continue;
+                }
+            }
+        }
+
+        if(s1.isEmpty() == true){
+            System.out.println("Balanced Stack");
+        }else{
+            System.out.println("Not Balanced stack");
+        }
+    }
 }
+
